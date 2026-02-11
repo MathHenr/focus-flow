fdc1ca3469a64fa310909c04f185c4b5983b3930
Matheus Henrique Sobrinho <matheusobrinho2k20@gmail.com>
2026-02-11 15:24:10 -0300

    feat: status display component logic

--- src/components/StatusDisplay/index.tsx
+++ src/components/StatusDisplay/index.tsx
@@ -1,14 +1,42 @@
+// import timer hook
+import { useTimerContext } from '../../hooks/useTimerContext';
+
 // style import
 import style from './style.module.css';
 
 // Lucide import
-import { BrainIcon } from 'lucide-react';
+import { BrainIcon, CoffeeIcon } from 'lucide-react';
 
 export function StatusDisplay() {
+  const { state } = useTimerContext();
   return (
-    <div className={style.content}>
-      <BrainIcon />
-      <p>Focus</p>
-    </div>
+    <>
+      {state.tasks.length === 0 ? (
+        <></>
+      ) : (
+        <div className={style.content}>
+          {state.tasks.map((_, index) => {
+            if (state.tasks[index].type === 'work') {
+              return (
+                <>
+                  <BrainIcon />
+                  <p>Focus</p>
+                </>
+              );
+            }
+            return (
+              <>
+                <CoffeeIcon />
+                {state.tasks[index].type === 'shortBreak' ? (
+                  <p>Short Break</p>
+                ) : (
+                  <p>Long Break</p>
+                )}
+              </>
+            );
+          })}
+        </div>
+      )}
+    </>
   );
 }
